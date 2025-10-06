0ffe0e39e67d2762f4fb93c2f318decb8a1f600e
Arnav Goyal <rockinarnav@hotmail.com>
2025-10-06 13:51:07 +0800

    codecrafters submit [skip ci]

---
+++ headers/Server.h
@@ -0,0 +1,75 @@
+#pragma once 
+#include <algorithm>
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string>
+#include <sys/select.h>
+#include <utility>
+#include <vector>
+#include <errno.h>
+#include <map>
+#include <chrono>
+
+
+class Server 
+{
+private:
+	
+	int server_fd; 
+	
+	fd_set masterfds;
+	
+	int maxfd;
+	
+	std::vector<int> clientfds;
+
+	char buffer[256];
+	
+	std::string response;
+	
+	std::string input;
+
+	std::vector<std::string> tokens; 
+			
+	std::map<std::string, std::string> dict;	
+	
+	std::map<std::string,
+	std::chrono::system_clock::time_point> times;
+
+	std::map<std::string, 
+	std::vector<std::string>> lists; 
+
+public:
+
+	Server();
+
+	~Server(); 
+
+	int getFD(); 
+
+	int reInit(); 
+
+	void getClients(); 
+
+	void getInput();
+
+	void commandCenter(); 
+
+	void setValue(); 
+	
+	void getValue(); 
+
+	void listPush();
+
+	void loop(); 
+};
---
+++ src/Server.cpp
@@ -0,0 +1,247 @@
+#include "../headers/Server.h"
+#include <string>
+#include <utility>
+#include <vector>
+
+Server::Server()
+{
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) 
+	{
+   std::cerr << "Failed to create server socket\n";
+  }
+  
+  // Since the tester restarts your program quite often,
+	// setting SO_REUSEADDR
+  // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse,
+								 sizeof(reuse)) < 0) 
+	{
+    std::cerr << "setsockopt failed\n";
+  }
+  
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(6379);
+  
+  if (bind(server_fd, (struct sockaddr *) &server_addr,
+					 sizeof(server_addr)) != 0) 
+	{
+    std::cerr << "Failed to bind to port 6379\n";
+  }
+  
+	int connection_backlog = 5;
+  if (listen(server_fd, connection_backlog) != 0) 
+	{
+    std::cerr << "listen failed\n";  
+  }
+}
+
+int Server::getFD()
+{
+	return server_fd;
+}
+
+int Server::reInit()
+{
+	FD_ZERO(&masterfds); 
+	FD_SET(server_fd, &masterfds);
+	maxfd = server_fd; 	
+	for (auto cfd : clientfds)
+	{	
+		FD_SET(cfd, &masterfds);
+		if (cfd > maxfd) maxfd = cfd; 
+	}
+	int activity =
+		select(maxfd + 1, &masterfds, NULL, NULL, NULL); 
+	
+	if (activity < 0) 
+	{
+		std::cerr << "select error\n"; 
+		std::cout << "Error code: " <<
+			errno << " Num of client fds " <<
+			clientfds.size() << "\n";  
+	}
+	return activity; 
+}
+
+void Server::getClients()
+{
+	if (FD_ISSET(server_fd, &masterfds))
+	{
+		struct sockaddr_in client_addr;
+		int client_addr_len = sizeof(client_addr);
+		int client_fd1 = accept(server_fd,
+												 (struct sockaddr *) &client_addr,
+													(socklen_t *) &client_addr_len);
+		
+		std::cout << "Client fd1: " <<  client_fd1 << "\n"; 	
+		if (client_fd1 < 0) 
+		{
+			std::cerr << "client 1 accept failed\n";			
+		}
+		clientfds.push_back(client_fd1);	
+		std::cout << "Added new client to set "
+			<< client_fd1 << "\n";
+	}
+}
+
+void Server::setValue()
+{	
+	std::pair<std::string, std::string> pairi =
+		{ tokens[2], tokens[3] };
+	
+	auto ret = dict.insert(pairi);
+	
+	if (ret.second == false)
+	{
+		std::cout << "Element already exists\n"; 
+	}
+	
+	if (tokens.size() > 4)
+	{
+		size_t p1 = tokens[5].find("\r\n",0);
+		int length = std::stoi(tokens[5].substr(0,p1));
+		int duration = std::stoi(tokens[5].substr(p1 + 2, length)); 
+		if (tokens[4] == "2\r\nEX\r\n") duration *= 1000;
+
+		std::chrono::milliseconds t(duration); 
+
+		std::chrono::system_clock::time_point time_limit
+			= std::chrono::system_clock::now() + t;
+
+		times.insert(std::pair<std::string,std::chrono::system_clock::time_point>(tokens[2],time_limit));
+	}
+
+	response = "+OK\r\n"; 
+}
+
+void Server::getValue()
+{
+	response = "$" + dict[tokens[2]];
+	auto it = times.find(tokens[2]); 
+	if (it != times.end())
+	{
+		auto now = std::chrono::system_clock::now(); 
+		if (it->second <= now)
+		{
+			std::cout << "Able to detect expiry\n";
+			dict.erase(tokens[2]);
+			times.erase(it); 
+			response = "$-1\r\n";
+		}
+	}
+}
+
+void Server::listPush()
+{
+	int n; 
+
+	auto it = lists.find(tokens[2]); 
+	if (it != lists.end())
+	{
+		it->second.push_back(tokens[3]); 
+		n = it->second.size();
+	}
+	else
+	{
+		lists.insert(
+			std::pair<std::string, std::vector<std::string>>
+			(tokens[2], std::vector<std::string>{ tokens[3] })
+		);
+		n = 1; 
+	}
+	response = ":" + std::to_string(n) + "\r\n";
+}
+
+void Server::commandCenter()
+{
+	if (tokens[1] == "4\r\nPING\r\n")
+	{
+		response = "+PONG\r\n";	
+	} 
+	else if (tokens[1] == "4\r\nECHO\r\n") 
+	{
+		response = "$" + tokens[2];
+	} 
+	else if (tokens[1] == "3\r\nSET\r\n")
+	{
+		setValue(); 		
+	} 
+	else if (tokens[1] == "3\r\nGET\r\n")
+	{
+		getValue(); 	
+	} 
+	else if (tokens[1] == "5\r\nRPUSH\r\n")
+	{
+		listPush();
+	}
+}
+
+void Server::getInput()
+{		
+	bzero(buffer, 256);
+
+	int i = 0; 
+
+	while (i < clientfds.size())
+	{ 
+		if (FD_ISSET(clientfds[i], &masterfds))
+		{
+			int num_bytes =
+				recv(clientfds[i], buffer, sizeof(buffer) - 1, 0);
+			
+			if (num_bytes == 0)
+			{
+				close(clientfds[i]);
+				std::cout << "Disconnected " << clientfds[i] << "\n"; 
+				clientfds.erase(clientfds.begin() + i); 
+				i--;
+				continue;
+			}
+
+			input = buffer; 
+			tokens.clear();
+			size_t start = 0; 
+			size_t end = input.find("$",start); 
+			while (end != std::string::npos)
+			{
+				tokens.push_back(input.substr(start,end - start));
+				start = end + 1; 
+				end = input.find("$", start); 
+			}
+			tokens.push_back(input.substr(start)); 
+			
+			commandCenter(); 
+								
+			int m = send(clientfds[i],
+								 response.c_str(),
+								 response.size(), 0);
+			if (m < 0) 
+			{
+				std::cerr << "Error in send\n"; 
+				std::printf("Socket error code %d\n", errno); 
+			}
+		} 
+		i++;
+	}
+}
+
+void Server::loop()
+{
+	while (true)
+	{		
+		if (reInit() < 0) break; 
+		getClients(); 
+		getInput();	
+	}
+}
+
+Server::~Server()
+{
+	std::cout << "Passed through\n";
+	clientfds.clear();
+	close(server_fd);
+}
